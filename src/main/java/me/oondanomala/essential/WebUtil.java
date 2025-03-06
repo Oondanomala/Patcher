e41e338cddff29927ecf8a514c1cdaf8909368ed
Oondanomala <87101222+Oondanomala@users.noreply.github.com>
2025-03-06 11:17:08 +0100

    Port WebUtil away from Essential

---
+++ src/main/java/me/oondanomala/essential/WebUtil.java
@@ -0,0 +1,33 @@
+package me.oondanomala.essential;
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+public final class WebUtil {
+    private WebUtil() {
+    }
+
+    public static String fetchString(String url) {
+        try {
+            HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+            connection.setRequestMethod("GET");
+            connection.addRequestProperty("User-Agent", Essential.modName + "/" + Essential.modVersion);
+            connection.setUseCaches(false);
+            connection.setDoOutput(true);
+            connection.setReadTimeout(10000);
+            connection.setConnectTimeout(10000);
+
+            try (InputStream stream = connection.getInputStream()) {
+                return IOUtils.toString(stream, StandardCharsets.UTF_8);
+            }
+        } catch (IOException e) {
+            Essential.logger.error("Failed to fetch from {}", url, e);
+            return null;
+        }
+    }
+}
