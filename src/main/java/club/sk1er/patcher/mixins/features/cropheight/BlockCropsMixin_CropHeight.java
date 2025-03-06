42350f586fedc57730233307b4d225187fd4b19d
Oondanomala <87101222+Oondanomala@users.noreply.github.com>
2025-03-06 11:18:40 +0100

    Port MinecraftUtil away from Essential

--- src/main/java/club/sk1er/patcher/mixins/features/cropheight/BlockCropsMixin_CropHeight.java
+++ src/main/java/club/sk1er/patcher/mixins/features/cropheight/BlockCropsMixin_CropHeight.java
@@ -2,8 +2,8 @@ package club.sk1er.patcher.mixins.features.cropheight;
 
 import club.sk1er.patcher.config.PatcherConfig;
 import club.sk1er.patcher.hooks.CropUtilities;
-import gg.essential.api.EssentialAPI;
 import gg.essential.universal.UMinecraft;
+import me.oondanomala.essential.MinecraftUtil;
 import net.minecraft.block.BlockCrops;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.BlockPos;
@@ -19,14 +19,14 @@ public abstract class BlockCropsMixin_CropHeight extends BlockMixin_CropHitbox {
     //#if MC==10809
     @Override
     public void getSelectedBoundingBox(World worldIn, BlockPos pos, CallbackInfoReturnable<AxisAlignedBB> cir) {
-        if (PatcherConfig.futureHitBoxes && (EssentialAPI.getMinecraftUtil().isHypixel() || UMinecraft.getMinecraft().isIntegratedServerRunning())) {
+        if (PatcherConfig.futureHitBoxes && (MinecraftUtil.isHypixel() || UMinecraft.getMinecraft().isIntegratedServerRunning())) {
             CropUtilities.updateCropsMaxY(worldIn, pos, worldIn.getBlockState(pos).getBlock());
         }
     }
 
     @Override
     public void collisionRayTrace(World worldIn, BlockPos pos, Vec3 start, Vec3 end, CallbackInfoReturnable<MovingObjectPosition> cir) {
-        if (PatcherConfig.futureHitBoxes && (EssentialAPI.getMinecraftUtil().isHypixel() || UMinecraft.getMinecraft().isIntegratedServerRunning())) {
+        if (PatcherConfig.futureHitBoxes && (MinecraftUtil.isHypixel() || UMinecraft.getMinecraft().isIntegratedServerRunning())) {
             CropUtilities.updateCropsMaxY(worldIn, pos, worldIn.getBlockState(pos).getBlock());
         }
     }
---
+++ src/main/java/me/oondanomala/essential/MinecraftUtil.java
@@ -0,0 +1,19 @@
+package me.oondanomala.essential;
+
+import net.minecraft.client.Minecraft;
+
+import java.util.Locale;
+
+public final class MinecraftUtil {
+    private MinecraftUtil() {
+    }
+
+    public static boolean isHypixel() {
+        Minecraft mc = Minecraft.getMinecraft();
+        if (mc.thePlayer == null || mc.theWorld == null || mc.isSingleplayer()) {
+            return false;
+        }
+        String brand = mc.thePlayer.getClientBrand();
+        return brand != null && brand.toLowerCase(Locale.ENGLISH).contains("hypixel");
+    }
+}
