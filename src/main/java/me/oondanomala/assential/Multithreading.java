946dc8a498223e38907cd23974e4b181cc088455
Oondanomala <87101222+Oondanomala@users.noreply.github.com>
2026-01-21 03:22:28 +0100

    Assential: avoid using Guava's ThreadFactoryBuilder

--- src/main/java/me/oondanomala/assential/Multithreading.java
+++ src/main/java/me/oondanomala/assential/Multithreading.java
@@ -1,12 +1,14 @@
 package me.oondanomala.assential;
 
-import com.google.common.util.concurrent.ThreadFactoryBuilder;
-
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public final class Multithreading {
-    private static final ExecutorService POOL = Executors.newCachedThreadPool(new ThreadFactoryBuilder().setNameFormat(Assential.modName + "-%d").build());
+    private static final AtomicInteger threadCounter = new AtomicInteger();
+    private static final ExecutorService POOL = Executors.newCachedThreadPool(
+        r -> new Thread(r, Assential.modName + "-" + threadCounter.getAndIncrement())
+    );
 
     private Multithreading() {
     }
